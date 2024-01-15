fdc67042949337b3314f291a4bf5cbc0bb05cc03
Denys <denisbadakaty51@gmail.com>
2024-01-15 16:09:13 +0200

    add signup page

--- src/components/auth/useLogic.ts
+++ src/components/auth/useLogic.ts
@@ -1,8 +1,12 @@
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 import { FormEvent, useRef, useState } from "react";
+import { createUser } from "./utils";
 
 export const useLogic = () => {
   const emailInputRef = useRef<HTMLInputElement | null>(null);
   const passwordInputRef = useRef<HTMLInputElement | null>(null);
+  const router = useRouter();
 
   const [isLogin, setIsLogin] = useState(true);
 
@@ -14,9 +18,29 @@ export const useLogic = () => {
     event: FormEvent<HTMLFormElement>
   ): Promise<void> => {
     event.preventDefault();
+    console.log(process.env.MONGODB_URL);
 
-    const enteredEmail = emailInputRef?.current?.value;
-    const enteredPassword = passwordInputRef?.current?.value;
+    // const enteredEmail = emailInputRef?.current?.value || "";
+    // const enteredPassword = passwordInputRef?.current?.value || "";
+
+    // if (isLogin) {
+    //   const result = await signIn("credentials", {
+    //     redirect: false,
+    //     email: enteredEmail,
+    //     password: enteredPassword,
+    //   });
+
+    //   if (!result!.error) {
+    //     router.replace("/profile");
+    //   }
+    // } else {
+    //   try {
+    //     const result = await createUser(enteredEmail, enteredPassword);
+    //     console.log(result);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
   };
 
   return {
